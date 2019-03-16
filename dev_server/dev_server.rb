cdcf732ffc81c24dc05e65951b79953b6e93855f
s.miyoshi <s.miyoshi1991@gmail.com>
2019-03-16 22:10:40 +0900

    refuctoring

--- dev_server/dev_server.rb
+++ dev_server/dev_server.rb
@@ -8,12 +8,12 @@ require 'fileutils'
 
 set :bind, '0.0.0.0'
 logger = Logger.new(STDOUT)
-isRealSend = false
+is_real_send = true
 
-if isRealSend
+if is_real_send
   # set SUBSCRIPTION_KEY in os environment variable with your valid subscription key.
-  subscriptionKey = ENV['SUBSCRIPTION_KEY']
-  unless subscriptionKey
+  subscription_key = ENV['SUBSCRIPTION_KEY']
+  unless subscription_key
     STDERR.puts('please set SUBSCRIPTION_KEY in os environment variable')
     exit 1
   end
@@ -23,6 +23,20 @@ get '/healthz' do
   return 'ok'
 end
 
+def send_ocr_request(url, subscription_key, image)
+  req = Net::HTTP::Post.new(url.request_uri)
+  req['Content-Type'] = 'application/octet-stream'
+  req['Ocp-Apim-Subscription-Key'] = subscription_key
+  req.body = image.read
+
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+  res = http.request(req)
+  res
+end
+
 # Input: image file
 # Output: title list(JSON format)
 post '/ocr' do
@@ -30,30 +44,21 @@ post '/ocr' do
 
   image = params[:photo][:tempfile]
   FileUtils.cp(image, 'send_image.jpg')
+  logger.info('file size: ' + File.size('send_image.jpg').to_s + '[byte]')
 
-  if isRealSend
+  if is_real_send
     uri = 'https://eastasia.api.cognitive.microsoft.com/vision/v2.0/ocr?language=ja'
     url = URI.parse(uri)
 
-    req = Net::HTTP::Post.new(url.request_uri)
-    req['Content-Type'] = 'application/octet-stream'
-    req['Ocp-Apim-Subscription-Key'] = subscriptionKey
-    req.body = image.read
-
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
     res = nil
     req_time = Benchmark.realtime do
-      res = http.request(req)
+      res = send_ocr_request(url, subscription_key, image)
     end
 
     logger.info('ocr time: ' + req_time.to_s + '[sec]')
 
     unless res.code.to_i >= 200 && res.code.to_i < 300
       logger.warn('request OCR failed(' + res.code + '): ' + res.message)
-      exit
     end
 
     return res.body
